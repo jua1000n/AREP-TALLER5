c332f039e647e7352c10f556ae48dbd30354f1f1
unknown <juan.cadavid-p@labinfo.is.escuelaing.edu.co>
2022-03-23 13:29:20 -0500

    Inicial

---
+++ src/main/java/SparkWebHello.java
@@ -0,0 +1,16 @@
+import static spark.Spark.*;
+
+public class SparkWebHello {
+    public static void main(String[] args) {
+        port(getPort());
+        get("/hello", (req, res) -> "Hello World");
+    }
+
+    private static int getPort() {
+
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000;
+    }
+}

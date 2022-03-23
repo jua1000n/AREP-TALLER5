fe6234931aa4507ed4861a8c8d581b3890cfa9e0
unknown <juan.cadavid-p@labinfo.is.escuelaing.edu.co>
2022-03-23 14:41:55 -0500

    ecicert

--- src/main/java/SparkWebHello.java
+++ src/main/java/SparkWebHello.java
@@ -2,6 +2,8 @@ import static spark.Spark.*;
 
 public class SparkWebHello {
     public static void main(String[] args) {
+        //API: secure(keystoreFilePath, keystorePassword, truststoreFilePath, truststorePassword);
+        secure(getKeyStore(), "123456", null, null);
         port(getPort());
         get("/hello", (req, res) -> "Hello World");
     }
@@ -11,5 +13,12 @@ public class SparkWebHello {
             return Integer.parseInt(System.getenv("PORT"));
         }
         return 5000;
+    }
+
+    private static String getKeyStore() {
+        if (System.getenv("KEYSTORE") != null) {
+            return System.getenv("KEYSTORE");
+        }
+        return "keystores/ecikeystore.p12";
     }
 }
